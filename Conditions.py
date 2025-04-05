a86fefb26b2e3c615dbde177c9591e7c32118219
Md. Asif Karim Chayon <79499779+9yon6yon-6yon@users.noreply.github.com>
2025-04-05 23:02:46 +0600

    Add files via upload

---
+++ Conditions.py
@@ -0,0 +1,62 @@
+# '''car conditions'''
+# rotationSwitch = True
+# breakPadel = False
+
+# headLight = False
+# tailLight = False
+
+
+# if rotationSwitch == True:
+#     headLight = True
+
+# if breakPadel == True:
+#     tailLight = True
+
+# print("Head light is : ", headLight)
+# print("Taile light is : ", tailLight)
+
+
+# '''city adventurer'''
+
+# skill = 15
+# if skill > 15:
+#     print("Welcome to the city 1.")
+# else:
+#     print("You can\'t enter to the city")
+
+
+# """if-elif-else statements"""
+# forwardButton = True
+# backwardButton = False
+
+# if forwardButton == True:
+#     motorStatus = 1
+#     print('Go forward')
+# elif backwardButton == True:
+#     motorStatus = 2
+#     print('Go backward')
+# else:
+#     motorStatus = 0
+#     print('Default state [Stopped]')
+
+# '''
+# Python executes code line-by-line
+# so the first condition
+# will run and stop if its 
+# true and won't go to other statement
+# '''
+
+
+# '''grade checking code'''
+# grade = int(input("Enter your grade: "))
+
+# if grade > 90:
+#     print('You got A+')
+# elif grade >= 85 and grade < 90:
+#     print('You got A')
+# elif grade >= 80 and grade < 85:
+#     print('You got A-')
+# elif grade >= 70 and grade < 80:
+#     print('You got B')
+# else:
+#     print('You have failed the exam')
---
+++ Variables.py
@@ -0,0 +1,29 @@
+# x = 2
+# y = 10
+
+# z = x + y
+# print(type(z))
+# print("The value of x + y is = ",z)
+
+# #integers, floats, strings <---- this is a single line comment
+
+# """
+# This is a multiple line comment
+# which can be used to clealy state any functions or other
+# properties of the code and system
+# """
+
+
+# '''
+# user inputs
+# '''
+# a = int(input('Enter the value of a: '))
+# b = int(input('Enter the value of b: '))
+# result = a + b
+# print('Value of a + b = ', result)
+
+
+# '''random number generator'''
+# import random
+# rand = random.randint(0,99)
+# print('Random number is: ',rand)
